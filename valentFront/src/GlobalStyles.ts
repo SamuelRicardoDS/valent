24c80e328f1d0d11b21710ab842e8c5fc1e655c3
SamuelRicardoDS <samuelricardods@gmail.com>
2023-06-27 18:52:31 -0300

    feat(styled components): background

--- valentFront/src/GlobalStyles.ts
+++ valentFront/src/GlobalStyles.ts
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import background from "./assets/background.png";
 
 export const GlobalStyles = createGlobalStyle`
   body {   
-    background-color: #111313; 
+    background-image: url(${background});
     color: #fff;
     font-family: 'Roboto', sans-serif;
     margin: 0;
