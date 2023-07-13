f77e7aaed50cd64c92518a006f8554f3f4d58333
SamuelRicardoDS <samuelricardods@gmail.com>
2023-07-13 11:15:24 -0300

    feat(): login page start

--- valentFront/src/Routes.tsx
+++ valentFront/src/Routes.tsx
@@ -2,6 +2,7 @@ import { Routes as ValentRoutes , Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Main } from "./pages/Main";
 import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
 
 export const Routes = () => {
   return (
@@ -9,6 +10,7 @@ export const Routes = () => {
       <Route path="/" Component={Home} />
       <Route path="/main" Component={Main} />
       <Route path="/user/sign-up" Component={SignUp} />
+      <Route path="/user/sign-in" Component={SignIn} />
     </ValentRoutes>
   );
 }
---
+++ valentFront/src/pages/SignIn.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import { SignInForm } from "../components/signIn/SignInForm";
+
+export const SignIn = () => {
+  return(
+    <div>
+      <Header />
+      <SignInForm />
+    </div>
+  )
+
+}
